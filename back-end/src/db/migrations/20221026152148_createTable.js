7204b138898fce167c9e3dd513a6e81bd56fafbb
Faisal KB <faisalkbarakat1@gmail.com>
2022-10-26 15:27:16 -0500

    revamped db, completed backend

--- back-end/src/db/migrations/20221026152148_createTable.js
+++ back-end/src/db/migrations/20221026152148_createTable.js
@@ -1,13 +1,14 @@
 exports.up = function (knex) {
     return knex.schema.createTable("tables", (table) => {
       table.increments("table_id").primary();
-      table.string("table_name").unique().notNullable();
-      table.integer("capacity").unsigned().notNullable();
+      table.string("table_name").notNullable();
+      table.integer("capacity").notNullable();
       table.integer("reservation_id").unsigned();
       table
         .foreign("reservation_id")
         .references("reservation_id")
-        .inTable("reservations");
+        .inTable("reservations")
+        .onDelete("cascade");
       table.timestamps(true, true);
     });
   };
--- back-end/src/db/seeds/01-tables.js
+++ back-end/src/db/seeds/01-tables.js
@@ -1,12 +1,7 @@
+const tables = require("./01-tables.json");
+
 exports.seed = function (knex) {
-    return knex
-      .raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE")
-      .then(function () {
-        return knex("tables").insert([
-          { table_name: "Bar #1", capacity: 1 },
-          { table_name: "Bar #2", capacity: 1 },
-          { table_name: "#1", capacity: 6 },
-          { table_name: "#2", capacity: 6 },
-        ]);
-      });
-  };
+  return knex.raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE").then(() => {
+    return knex("tables").insert(tables);
+  });
+};
